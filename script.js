533c0bb88cc005a981e5580c33bfbaa23b53db0e
brunalozinski <lozinskibruna@gmail.com>
2021-08-03 11:26:12 -0300

    commit

--- script.js
+++ script.js
@@ -1,22 +1,52 @@
 const numero1 = document.getElementById("numero1")
 const numero2 = document.getElementById("numero2")
 const resultado = document.getElementById("resultado")
+
 const button = document.getElementById("somar")
 const button = document.getElementById("subtrair")
 const button = document.getElementById("multiplicar")
 const button = document.getElementById("dividir")
 
+const formulario = document.getElementById("formulario")
+
 function soma() {
-    resultado.value = parseInt (numero1.value) + parseInt (numero2.value)
+    if (numero1.value !== '' && numero2.value !== '') {
+        resultado.value = parseInt (numero1.value) + parseInt (numero2.value)
+
+        formulario.classList.add("somar")
+    } else {
+        alert("Preencha todos os campos solicitados!")
+    }
 }
+
 function subtracao() {
-    resultado.value = parseInt (numero1.value) - parseInt (numero2.value)
+    if (numero1.value !== '' && numero2.value !== '') {
+        resultado.value = parseInt (numero1.value) - parseInt (numero2.value)
+        
+        formulario.classList.add("subtrair")
+    } else {
+        alert("Preencha todos os campos solicitados!")
+    }
 }
+
 function multiplicacao() {
-    resultado.value = parseInt (numero1.value) * parseInt (numero2.value)
+    if (numero1.value !== '' && numero2.value !== '') {
+        resultado.value = parseInt (numero1.value) * parseInt (numero2.value)
+        
+        formulario.classList.add("multiplicar")
+    } else {
+        alert("Preencha todos os campos solicitados!")
+    }
 }
+
 function divisao() {
-    resultado.value = parseInt (numero1.value) / parseInt (numero2.value)
+    if (numero1.value !== '' && numero2.value !== '') {
+        resultado.value = parseInt (numero1.value) / parseInt (numero2.value)
+
+        formulario.classList.add("dividir")
+    } else {
+        alert("Preencha todos os campos solicitados!")
+    }
 }
 
 button.addEventListener("click", soma)
